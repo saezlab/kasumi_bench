848b9363c3f920b5b4fbfe518dd68ba168ca4d39
Jovan Tanevski <tanevski@gmail.com>
2023-04-05 16:30:58 +0200

    utils+celltype benchmark

---
+++ benchmark_celltype.R
@@ -0,0 +1,359 @@
+source("utils.R")
+
+
+plan(multisession, workers = 4)
+
+
+# MIBI  ----
+
+## DCIS progression ----
+
+dcis <- read_csv("data/DCISMIBI/Single Cell Data/Single_Cell_Data.csv")
+
+# can also be dcis$manual_gating_subineage %>% unique()
+cts <- dcis$sublineage %>% unique()
+resp <- read_csv("data/DCISMIBI/Tissue Feature Data/Table_S1_Patient_Feature_Table.csv")
+
+points <- dcis %>%
+  filter(Tissue_Type == "DCIS") %>%
+  pull(Point_Num) %>%
+  unique() %>%
+  intersect(resp %>% filter(Status %in% c("progressor", "nonprogressor")) %>% pull(PointNumber))
+
+all.cells.dcis <- points %>% map(\(id){
+  dcis %>%
+    filter(Point_Num == id) %>%
+    pull(sublineage) %>%
+    map(~ .x == cts) %>%
+    rlist::list.rbind() %>%
+    `colnames<-`(make.names(cts)) %>%
+    as_tibble()
+})
+
+names(all.cells.dcis) <- points
+
+all.positions.dcis <- points %>% map(\(id){
+  dcis %>%
+    filter(Point_Num == id) %>%
+    select(label) %>%
+    left_join(read_csv(paste0("data/DCISMIBI/Image Data/Segmetation_Outlines_and_Labels_Mendeley/", id, ".csv"), show_col_types = FALSE), by = "label") %>%
+    select("centroid-0", "centroid-1") %>%
+    `colnames<-`(c("x", "y"))
+})
+
+## SM DCIS ----
+
+misty.results <- sm_train(all.cells.dcis, all.positions.dcis, 10, 150, 20, 3, "DCIStest")
+
+# IDEA remove variables from representation not present in at least p% of samples
+
+param.opt <- optimal_smclust(misty.results, resp %>% select(PointNumber, Status) %>%
+  rename(id = PointNumber, target = Status) %>%
+  filter(target %in% c("progressor", "nonprogressor")) %>%
+  mutate(id = as.character(id), target = as.factor(target)))
+
+sm.repr <- sm_labels(misty.results, cuts = param.opt["cut"], res = param.opt["res"])
+sm.repr <- sm_labels(misty.results, cuts = 0.9, res = 0.5)
+
+repr.ids <- sm.repr %>% map_chr(~ .x$id[1])
+
+freq.sm <- sm.repr %>%
+  map_dfr(~ .x %>%
+    select(-c(id, x, y)) %>%
+    freq_repr()) %>%
+  select(where(~ sd(.) > 1e-3)) %>%
+  add_column(id = repr.ids) %>%
+  left_join(resp %>% select(PointNumber, Status) %>% mutate(PointNumber = as.character(PointNumber)), by = c("id" = "PointNumber")) %>%
+  rename(target = Status) %>%
+  mutate(target = as.factor(target)) %>%
+  select(-id)
+
+# 0.705
+roc.sm <- classify(freq.sm)
+
+
+cn.results <- cn_train(all.cells.dcis, all.positions.dcis)
+cn.repr <- cn_labels(cn.results, k = 17)
+
+repr.ids.cn <- cn.repr %>% map_chr(~ .x$id[1])
+
+freq.cn <- cn.repr %>%
+  map_dfr(~ .x %>%
+    select(-c(id, x, y)) %>%
+    freq_repr()) %>%
+  select(where(~ sd(.) > 1e-3)) %>%
+  add_column(id = repr.ids.cn) %>%
+  filter(id %in% repr.ids) %>%
+  left_join(resp %>% select(PointNumber, Status) %>% mutate(PointNumber = as.character(PointNumber)), by = c("id" = "PointNumber")) %>%
+  rename(target = Status) %>%
+  mutate(target = as.factor(target)) %>%
+  select(-id)
+
+# 0.502
+roc.cn <- classify(freq.cn)
+
+freq.dcis <- all.cells.dcis %>%
+  map(freq_repr) %>%
+  list_transpose() %>%
+  as_tibble() %>%
+  mutate(PointNumber = points) %>%
+  left_join(resp %>% select(PointNumber, Status), by = "PointNumber") %>%
+  filter(PointNumber %in% repr.ids) %>%
+  select(-PointNumber) %>%
+  rename(target = Status) %>%
+  mutate(target = as.factor(target))
+
+# 0.448
+roc.fr <- classify(freq.dcis)
+
+ggroc(list(sm = roc.sm, cn = roc.cn, fr = roc.fr), legacy.axes = TRUE) + 
+  geom_abline(intercept = 0, slope = 1, color = "gray50", linetype="dotted") + 
+  labs(color = NULL) +
+  ggtitle("MIBI DCIS") + theme_classic()
+
+ggsave("roc.mibi.pdf")
+
+markov.dcis <- all.cells.dcis %>%
+  map2(all.positions.dcis, markov_repr) %>%
+  list_transpose() %>%
+  as_tibble(.name_repair = "unique") %>%
+  mutate(PointNumber = points) %>%
+  left_join(resp %>% select(PointNumber, Status), by = "PointNumber") %>%
+  filter(PointNumber %in% repr.ids) %>%
+  select(-PointNumber) %>%
+  rename(target = Status) %>%
+  mutate(target = as.factor(target))
+
+classify(markov.dcis)
+
+
+# CODEX ----
+
+## CTCL responders  ----
+
+lymph <- read_csv("data/LymphomaCODEX/single_cells.csv")
+spots <- lymph %>%
+  filter(Groups %in% c(1, 2)) %>%
+  pull(Spots) %>%
+  unique()
+outcome <- lymph %>%
+  filter(Groups %in% c(1, 2)) %>%
+  group_by(Spots, Patients) %>%
+  summarize(Groups = Groups[1], .groups = "drop")
+
+cts <- lymph %>%
+  pull(ClusterName) %>%
+  unique()
+
+all.cells.lymph <- spots %>% map(\(id){
+  lymph %>%
+    filter(Spots == id) %>%
+    pull(ClusterName) %>%
+    map(~ .x == cts) %>%
+    rlist::list.rbind() %>%
+    `colnames<-`(make.names(cts)) %>%
+    as_tibble()
+})
+
+names(all.cells.lymph) <- spots
+
+all.positions.lymph <- spots %>% map(\(id){
+  lymph %>%
+    filter(Spots == id) %>%
+    select(X, Y) %>%
+    `colnames<-`(c("x", "y"))
+})
+
+
+## SM CTCL ----
+
+# 10 neighbors as in publication, 200px = 75um window
+misty.results <- sm_train(all.cells.lymph, all.positions.lymph, 10, 200, 20, 3, "CTCLtest")
+
+param.opt <- optimal_smclust(misty.results, outcome %>% select(-Patients) %>%
+  rename(id = Spots, target = Groups) %>%
+  mutate(id = as.character(id), target = as.factor(make.names(target))))
+
+# 0.6, 0.8 for 5fold
+# 0.8, 0.7 for 10fold
+sm.repr <- sm_labels(misty.results, cuts = param.opt["cut"], res = param.opt["res"])
+#sm.repr <- sm_labels(misty.results, cuts = 0.8, res = 0.7)
+
+repr.ids <- sm.repr %>% map_chr(~ .x$id[1])
+
+freq.sm <- sm.repr %>%
+  map_dfr(~ .x %>%
+    select(-c(id, x, y)) %>%
+    freq_repr()) %>%
+  select(where(~ sd(.) > 1e-3)) %>%
+  add_column(id = repr.ids) %>%
+  left_join(outcome %>% mutate(Spots = as.character(Spots)), by = c("id" = "Spots")) %>%
+  rename(target = Groups) %>%
+  mutate(target = as.factor(make.names(target))) %>%
+  select(-id, -Patients)
+
+# 0.775
+roc.sm <- classify(freq.sm)
+
+
+
+cn.results <- cn_train(all.cells.lymph, all.positions.lymph)
+cn.repr <- cn_labels(cn.results, k = 10)
+
+repr.ids.cn <- cn.repr %>% map_chr(~ .x$id[1])
+
+freq.cn <- cn.repr %>%
+  map_dfr(~ .x %>%
+    select(-c(id, x, y)) %>%
+    freq_repr()) %>%
+  select(where(~ sd(.) > 1e-3)) %>%
+  add_column(id = repr.ids.cn) %>%
+  filter(id %in% repr.ids) %>%
+  left_join(outcome %>% mutate(Spots = as.character(Spots)), by = c("id" = "Spots")) %>%
+  rename(target = Groups) %>%
+  mutate(target = as.factor(make.names(target))) %>%
+  select(-id, -Patients)
+
+# 0.625
+roc.cn <- classify(freq.cn)
+
+
+freq.lymph <- all.cells.lymph %>%
+  map(freq_repr) %>%
+  list_transpose() %>%
+  as_tibble() %>%
+  mutate(Spots = spots) %>%
+  left_join(outcome, by = "Spots") %>%
+  filter(Spots %in% repr.ids) %>%
+  select(-Spots) %>%
+  rename(target = Groups) %>%
+  mutate(target = as.factor(make.names(target)))
+
+
+# 0.4875
+roc.fr <- classify(freq.lymph)
+
+ggroc(list(sm = roc.sm, cn = roc.cn, fr = roc.fr), legacy.axes = TRUE) + 
+  geom_abline(intercept = 0, slope = 1, color = "gray50", linetype="dotted") + 
+  labs(color = NULL) +
+  ggtitle("CODEX CTCL") + theme_classic()
+
+ggsave("roc.codex.pdf")
+
+# IMC ----
+
+## BC responders ----
+bmeta <- read_csv("data/BCIMC/Basel_PatientMetadata.csv")
+cores <- bmeta %>%
+  filter(
+    diseasestatus == "tumor",
+    response %in% c("Sensitive", "Resistant"),
+    clinical_type == "HR+HER2-", Subtype == "PR+ER+"
+  ) %>%
+  pull(core)
+bclusters <- read_csv("data/BCIMC/Cluster_labels/Basel_metaclusters.csv") %>%
+  mutate(core = str_remove(id, "_\\d+$")) %>%
+  filter(core %in% cores)
+scloc <- read_csv("data/BCIMC/singlecell_locations/Basel_SC_locations.csv") %>%
+  filter(core %in% cores)
+
+cts <- bclusters %>%
+  pull(cluster) %>%
+  unique()
+
+all.cells.bc <- cores %>% map(\(clus){
+  bclusters %>%
+    filter(core == clus) %>%
+    pull(cluster) %>%
+    map(~ .x == cts) %>%
+    rlist::list.rbind() %>%
+    `colnames<-`(make.names(cts)) %>%
+    as_tibble()
+})
+
+names(all.cells.bc) <- cores
+
+all.positions.bc <- cores %>% map(\(clus){
+  scloc %>%
+    filter(core == clus) %>%
+    select(Location_Center_X, Location_Center_Y) %>%
+    `colnames<-`(c("x", "y"))
+})
+
+
+## SM BC ----
+
+# change others to minm=2?
+misty.results <- sm_train(all.cells.bc, all.positions.bc, 10, 100, 20, 2, "BCtest")
+
+resp <- bmeta %>%
+  filter(
+    diseasestatus == "tumor",
+    response %in% c("Sensitive", "Resistant"),
+    clinical_type == "HR+HER2-", Subtype == "PR+ER+"
+  ) %>%
+  select(core, response)
+
+param.opt <- optimal_smclust(misty.results, resp %>%
+  rename(id = core, target = response) %>%
+  mutate(id = as.character(id), target = as.factor(make.names(target))))
+
+#sm.repr <- sm_labels(misty.results, cuts = param.opt["cut"], res = param.opt["res"])
+sm.repr <- sm_labels(misty.results, cuts = 0.5, res = 0.8)
+
+repr.ids <- sm.repr %>% map_chr(~ .x$id[1])
+
+freq.sm <- sm.repr %>%
+  map_dfr(~ .x %>%
+    select(-c(id, x, y)) %>%
+    freq_repr()) %>%
+  select(where(~ sd(.) > 1e-3)) %>%
+  add_column(id = repr.ids) %>%
+  left_join(resp, by = c("id" = "core")) %>%
+  rename(target = response) %>%
+  mutate(target = as.factor(make.names(target))) %>%
+  select(-id)
+
+roc.sm <- classify(freq.sm)
+
+cn.results <- cn_train(all.cells.bc, all.positions.bc)
+cn.repr <- cn_labels(cn.results, k = 6)
+
+repr.ids.cn <- cn.repr %>% map_chr(~ .x$id[1])
+
+freq.cn <- cn.repr %>%
+  map_dfr(~ .x %>%
+            select(-c(id, x, y)) %>%
+            freq_repr()) %>%
+  select(where(~ sd(.) > 1e-3)) %>%
+  add_column(id = repr.ids.cn) %>%
+  filter(id %in% repr.ids) %>%
+  left_join(resp, by = c("id" = "core")) %>%
+  rename(target = response) %>%
+  mutate(target = as.factor(make.names(target))) %>%
+  select(-id)
+
+# 0.4475
+roc.cn <- classify(freq.cn)
+
+freq.bc <- all.cells.bc %>%
+  map(freq_repr) %>%
+  list_transpose() %>%
+  as_tibble() %>%
+  mutate(core = cores) %>%
+  filter(core %in% repr.ids) %>%
+  left_join(resp, by = "core") %>%
+  rename(target = response) %>%
+  mutate(target = as.factor(make.names(target))) %>%
+  select(-core)
+
+# 0.509
+roc.fr <- classify(freq.bc)
+
+ggroc(list(sm = roc.sm, cn = roc.cn, fr = roc.fr), legacy.axes = TRUE) + 
+  geom_abline(intercept = 0, slope = 1, color = "gray50", linetype="dotted") + 
+  labs(color = NULL) +
+  ggtitle("IMC BC") + theme_classic()
+
+ggsave("roc.imc.pdf")
+
---
+++ utils.R
@@ -0,0 +1,277 @@
+library(mistyR)
+library(future)
+library(tidyverse)
+library(furrr)
+library(uwot)
+library(knn.covertree)
+library(proxy)
+library(igraph)
+library(readxl)
+library(ranger)
+library(caret)
+library(pROC)
+
+
+# these two functions for highest level first and second order representation per slide
+# should work in general for any labeling (binary) of the samples
+# labels can be: cell types, cell neighborhoods, sliding misty labels
+freq_repr <- function(labels) {
+  colSums(labels) / sum(labels)
+}
+
+
+markov_repr <- function(labels, positions) {
+  dist1 <- find_knn(positions, 1)$dist
+  threshold <- 2 * sd(dist1) + mean(dist1)
+  misty.views <- create_initial_view(labels) %>%
+    add_juxtaview(positions, neighbor.thr = threshold)
+  suppressMessages(
+    # instead of simply summing, it might be worth dividing by the number of cell type 1
+    neighb <- misty.views[[paste0("juxtaview.", threshold)]]$data %>%
+      mutate(id = apply(labels, 1, which)) %>%
+      add_row(id = seq_len(ncol(labels))) %>%
+      replace(is.na(.), 0) %>%
+      group_by(id) %>%
+      group_modify(~ colSums(.x) %>% as_tibble_row()) %>%
+      ungroup() %>%
+      select(-id) # %>%
+    # apply(1, \(x) x/sum(x)) %>%
+    # replace(is.na(.), 0) %>% t()
+  )
+
+  # calculate probabilities per row and serialize the whole matrix
+  # neighb %>% as.numeric()
+  # if not normalized per row
+  serialized <- neighb[upper.tri(neighb, diag = TRUE)]
+  serialized / sum(serialized)
+}
+
+# representation can be cns or sliding misty signatures
+leiden_onsnn <- function(representation, nn = 30, minjac = 0.15, resolution = 0.8) {
+  neighbors <- find_knn(representation, nn, distance = "cosine")
+  snn <- simil(neighbors$index, "eJaccard")
+  snn[snn < minjac] <- 0
+
+  groups <-
+    graph.adjacency(snn %>% as.matrix(), mode = "undirected", weighted = TRUE) %>%
+    cluster_leiden(resolution_parameter = resolution, n_iterations = -1)
+
+  return(groups$membership)
+}
+
+leiden_onsim <- function(representation, minsim = 0.8, resolution = 0.8, measure = "cosine") {
+  sim <- simil(representation, measure)
+
+  sim[sim < minsim] <- 0
+
+  groups <-
+    graph.adjacency(sim %>% as.matrix(), mode = "undirected", weighted = TRUE) %>%
+    cluster_leiden(resolution_parameter = resolution, n_iterations = -1)
+
+  return(groups$membership)
+}
+
+kmeans_ondist <- function(representation, k = 10) {
+  clust <- ClusterR::KMeans_rcpp(representation, k)
+  return(clust$clusters)
+}
+
+# cns should be calculated on all samples
+# names(all.cells) used as sample ids
+cn_train <- function(all.cells, all.positions) {
+  concat <- seq_along(all.cells) %>% map_dfr(\(i){
+    misty.views <- create_initial_view(all.cells[[i]]) %>%
+      add_paraview(all.positions[[i]], 10, family = "constant", cache = TRUE)
+    misty.views[["paraview.10"]]$data %>%
+      add_column(id = names(all.cells)[i]) %>%
+      cbind(all.positions[[i]])
+  })
+
+  return(concat)
+}
+
+
+cn_labels <- function(neighborhoods, k) {
+  # clusters <- leiden_onsim(neighborhoods %>% select(-c(id, x, y)), cut, res)
+  clusters <- kmeans_ondist(neighborhoods %>% select(-c(id, x, y)), k)
+  samps <- neighborhoods %>% select(id, x, y)
+
+
+  # return one-hot representation for all all.cells and if needed all.positions
+  suppressMessages(
+    cn.repr <- map(clusters, ~ .x == seq(length(unique(clusters)))) %>% reduce(rbind) %>%
+      as_tibble(.name_repair = "unique") %>% cbind(samps) %>% group_by(id) %>%
+      group_split()
+  )
+
+  return(cn.repr)
+}
+
+
+sm_train <- function(all.cells, all.positions, l, window, minu, minm, top.folder) {
+  if (file.exists(paste0(top.folder, ".rds"))) {
+    misty.results <- read_rds(paste0(top.folder, ".rds"))
+  } else {
+    outputs <- seq_along(all.cells) %>%
+      map(\(i){
+        if (dir.exists(paste0(top.folder, "/sample", names(all.cells)[i], "/"))) {
+          folders <- list.dirs(paste0(top.folder, "/sample", names(all.cells)[i], "/"))[-1]
+        } else {
+          misty.views <- create_initial_view(all.cells[[i]]) %>%
+            add_paraview(all.positions[[i]], l,
+              family = "constant", cached = TRUE,
+              prefix = "p."
+            )
+
+          folders <- run_sliding_misty(misty.views, all.positions[[i]], window,
+            minu = minu, minm = minm,
+            results.folder = paste0(top.folder, "/sample", names(all.cells)[i], "/"),
+            bypass.intra = TRUE
+          )
+        }
+
+        ifelse(length(folders) >= 10, return(folders), return(NA))
+      }) %>%
+      unlist()
+
+    misty.results <- collect_results(outputs[!is.na(outputs)])
+    write_rds(misty.results, paste0(top.folder, ".rds"), "gz")
+  }
+  return(misty.results)
+}
+
+misty_train <- function(all.cells, all.positions, l, top.folder) {
+  if (file.exists(paste0(top.folder, "_ws.rds"))) {
+    misty.results <- read_rds(paste0(top.folder, "_ws.rds"))
+  } else {
+    outputs <- seq_along(all.cells) %>%
+      map(\(i){
+        misty.views <- create_initial_view(all.cells[[i]]) %>%
+          add_paraview(all.positions[[i]], l,
+            family = "constant", cached = TRUE,
+            prefix = "p."
+          )
+
+        run_misty(misty.views,
+          results.folder = paste0(top.folder, "_ws/sample", names(all.cells)[i], "/"),
+          bypass.intra = TRUE
+        )
+      }) %>%
+      unlist()
+
+    misty.results <- collect_results(outputs)
+    write_rds(misty.results, paste0(top.folder, "_ws.rds"), "gz")
+  }
+  return(misty.results)
+}
+
+sm_labels <- function(misty.results, cuts, res, cutoff = 0) {
+  # trimming matters
+  sig <- extract_signature(misty.results, type = "i", intersect.targets = FALSE)
+  sig[is.na(sig)] <- floor(min(sig %>% select(-sample), na.rm = TRUE))
+  sig <- sig %>% mutate(across(!sample, ~ ifelse(.x <= cutoff, 0, .x)))
+
+  keep <- which(sig %>% select(-sample, -contains("intra_")) %>% rowSums() != 0)
+
+  samps <- sig %>%
+    slice(keep) %>%
+    select(sample) %>%
+    mutate(
+      id = str_extract(sample, "sample.*/") %>% str_remove("/") %>% str_remove("^sample"),
+      box = str_extract(sample, "/[0-9].*$") %>% str_remove("/")
+    ) %>%
+    rowwise(id) %>%
+    summarize(rebox = box %>%
+      str_split("_", simplify = T) %>%
+      as.numeric() %>% list(), .groups = "drop") %>%
+    rowwise(id) %>%
+    summarize(
+      xcenter = (rebox[3] + rebox[1]) / 2,
+      ycenter = (rebox[4] + rebox[2]) / 2, .groups = "drop"
+    )
+
+  # the filtering here also matters
+  clean <- sig %>%
+    select(-sample, -contains("intra_")) %>%
+    slice(keep) %>%
+    select(where(~ sum(.) != 0 & !all(. <= 0)))
+
+  # think about gain.R2 as node weights
+  clusters <- leiden_onsim(clean, cuts, res)
+
+  suppressMessages(
+    sm.repr <- map(clusters, ~ .x == seq(length(unique(clusters)))) %>% reduce(rbind) %>%
+      as_tibble(.name_repair = "unique") %>% cbind(samps) %>% group_by(id) %>% rename(x = xcenter, y = ycenter) %>%
+      group_split()
+  )
+
+  return(sm.repr)
+}
+
+misty_labels <- function(misty.results) {
+
+}
+
+
+# the column target in the representation table is the ground truth
+# returns ROC based on 10-fold cv predictions
+classify <- function(representation, plot = FALSE) {
+  set.seed(1)
+  suppressWarnings(
+    model <- train(target ~ ., representation,
+      method = "glm", metric = "ROC",
+      trControl = trainControl(
+        method = "cv", number = 10,
+        classProbs = TRUE,
+        summaryFunction = twoClassSummary,
+        savePredictions = TRUE
+      )
+    )
+  )
+
+  roc(model$pred$obs, model$pred[, 3], quiet = TRUE)
+}
+
+# the column target in the representation table is the ground truth
+# returns macro F1 per class based on 10-fold cv predictions
+classify_rf <- function(representation) {
+  model <- ranger(target ~ ., representation,
+    seed = 1,
+    classification = TRUE, probability = TRUE
+  )
+
+  # perf <- confusionMatrix(model$predictions, representation$target)
+  #
+  # if (is.matrix(perf$byClass)) {
+  #   perf$byClass[, "F1"] %>%
+  #     replace_na(0) %>%
+  #     mean()
+  # } else {
+  #   perf$byClass["F1"]
+  # }
+
+  roc(representation$target, model$predictions[, 1], quiet = TRUE, smooth = TRUE, smooth.n = 10)
+}
+
+optimal_smclust <- function(misty.results, true.labels, funct = classify) {
+  grid <- seq(0.5, 0.9, 0.1) %>% map_dfr(\(cuts){
+    seq(0.5, 0.9, 0.1) %>% map_dfr(\(res){
+      sm.repr <- sm_labels(misty.results, cuts, res)
+
+      repr.ids <- sm.repr %>% map_chr(~ .x$id[1])
+
+      perf <- try(
+        sm.repr %>% map_dfr(~ .x %>%
+          select(-c(id, x, y)) %>%
+          freq_repr()) %>%
+          select(where(~ sd(.) > 1e-3)) %>%
+          add_column(id = repr.ids) %>% left_join(true.labels, by = "id") %>%
+          drop_na() %>% select(-id) %>% funct()
+      )
+
+      print(paste(cuts, res, as.numeric(perf$auc)))
+      tibble_row(cut = cuts, res = res, perf = as.numeric(perf$auc))
+    })
+  })
+  grid[which.max(grid$perf), ] %>% unlist()
+}
