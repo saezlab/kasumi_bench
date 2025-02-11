9d8c4e98fe655092c81627c2234d7dbb31f5b0ae
Jovan Tanevski <tanevski@gmail.com>
2025-02-11 15:15:20 +0100

    pdac update

--- run_pdac_smi.R
+++ run_pdac_smi.R
@@ -5,11 +5,8 @@ source("utils.R")
 plan(multisession, workers = 8)
 options(future.globals.maxSize = 2024^3)
 
-# Download data ----
-if (!file.exists("raw_meta_data_final.h5ad")) {
-  download.file("https://data.mendeley.com/public-files/datasets/kx6b69n3cb/files/26999a73-4b05-4f47-9cab-023cd0de80a9/file_downloaded", "raw_meta_data_final.h5ad", method = "libcurl")
-}
-all <- read_h5ad("raw_meta_data_final.h5ad")
+# Read data ----
+all <- read_h5ad("data/PDACSMI/raw_meta_data_final.h5ad")
 
 all.feat <- all$obs %>%
   filter(treatment_status %in% c("CRT", "Untreated")) %>%
@@ -54,11 +51,11 @@ all.cells <- all.feat %>%
   dplyr::select(annotation_subtypes) %>%
   group_split(.keep = FALSE) %>%
   map(\(samp) samp %>%
-        pull(annotation_subtypes) %>%
-        map(~ .x == cts) %>%
-        rlist::list.rbind() %>%
-        `colnames<-`(make.names(cts)) %>%
-        as_tibble())
+    pull(annotation_subtypes) %>%
+    map(~ .x == cts) %>%
+    rlist::list.rbind() %>%
+    `colnames<-`(make.names(cts)) %>%
+    as_tibble())
 
 names(all.cells) <- ids
 
@@ -107,6 +104,7 @@ roc.sm$auc
 # Model reliance ----
 
 mr <- model_reliance(freq.sm)
+ggsave("PDACreliance.pdf", height = 6, width = 5)
 
 # Kasumi representation UMAP ----
 
@@ -121,6 +119,7 @@ data.frame(kasumi.umap[, 1:2]) %>%
   geom_point() +
   theme_classic()
 
+ggsave("PDACumap.pdf", height = 5, width = 6)
 
 # Heatmaps ----
 
@@ -129,40 +128,58 @@ sm.repr.ext <- sm_labels(kasumi.results, 0.6, 0.6, freq = FALSE)
 kasumi.cluster.13 <- describe_cluster(sm.repr.ext, 13, "PDACct1200.sqm")
 
 plot_improvement_stats(kasumi.cluster.13, trim = 1)
+ggsave("clusterfigs/pdac13.pdf", width = 4, height = 4)
 plot_interaction_heatmap(kasumi.cluster.13, "paraview.10", trim = 1, clean = TRUE)
+ggsave("clusterfigs/pdac13h.pdf", width = 5, height = 4)
 plot_interaction_heatmap(kasumi.cluster.13, "paraview.10", trim = 1, clean = TRUE, correlation = TRUE)
+ggsave("clusterfigs/pdac13c.pdf", width = 5, height = 4)
 
 kasumi.cluster.1 <- describe_cluster(sm.repr.ext, 1, "PDACct1200.sqm")
 
 plot_improvement_stats(kasumi.cluster.1, trim = 1)
+ggsave("clusterfigs/pdac1.pdf", width = 4, height = 4)
 plot_interaction_heatmap(kasumi.cluster.1, "paraview.10", trim = 1, clean = TRUE)
+ggsave("clusterfigs/pdac1h.pdf", width = 5, height = 3)
 plot_interaction_heatmap(kasumi.cluster.1, "paraview.10", trim = 1, clean = TRUE, correlation = TRUE)
+ggsave("clusterfigs/pdac1c.pdf", width = 5, height = 3)
 
 kasumi.cluster.3 <- describe_cluster(sm.repr.ext, 3, "PDACct1200.sqm")
 
 plot_improvement_stats(kasumi.cluster.3, trim = 1)
+ggsave("clusterfigs/pdac3.pdf", width = 4, height = 4)
 plot_interaction_heatmap(kasumi.cluster.3, "paraview.10", trim = 1, clean = TRUE)
+ggsave("clusterfigs/pdac3h.pdf", width = 5, height = 3)
 plot_interaction_heatmap(kasumi.cluster.3, "paraview.10", trim = 1, clean = TRUE, correlation = TRUE)
+ggsave("clusterfigs/pdac3c.pdf", width = 5, height = 3)
 
 
 
 kasumi.cluster.30 <- describe_cluster(sm.repr.ext, 30, "PDACct1200.sqm")
 
 plot_improvement_stats(kasumi.cluster.30, trim = 1)
+ggsave("clusterfigs/pdac30.pdf", width = 4, height = 4)
 plot_interaction_heatmap(kasumi.cluster.30, "paraview.10", trim = 1, clean = TRUE)
+ggsave("clusterfigs/pdac30h.pdf", width = 5, height = 3)
 plot_interaction_heatmap(kasumi.cluster.30, "paraview.10", trim = 1, clean = TRUE, correlation = TRUE)
+ggsave("clusterfigs/pdac30c.pdf", width = 5, height = 3)
 
 kasumi.cluster.116 <- describe_cluster(sm.repr.ext, 116, "PDACct1200.sqm")
 
 plot_improvement_stats(kasumi.cluster.116, trim = 1)
+ggsave("clusterfigs/pdac116.pdf", width = 4, height = 4)
 plot_interaction_heatmap(kasumi.cluster.116, "paraview.10", trim = 1, clean = TRUE)
+ggsave("clusterfigs/pdac116h.pdf", width = 5, height = 4)
 plot_interaction_heatmap(kasumi.cluster.116, "paraview.10", trim = 1, clean = TRUE, correlation = TRUE)
+ggsave("clusterfigs/pdac116c.pdf", width = 5, height = 4)
 
 kasumi.cluster.18 <- describe_cluster(sm.repr.ext, 18, "PDACct1200.sqm")
 
 plot_improvement_stats(kasumi.cluster.18, trim = 1)
+ggsave("clusterfigs/pdac18.pdf", width = 4, height = 4)
 plot_interaction_heatmap(kasumi.cluster.18, "paraview.10", trim = 1, clean = TRUE)
+ggsave("clusterfigs/pdac18h.pdf", width = 5, height = 4)
 plot_interaction_heatmap(kasumi.cluster.18, "paraview.10", trim = 1, clean = TRUE, correlation = TRUE)
+ggsave("clusterfigs/pdac18c.pdf", width = 5, height = 4)
 
 
 
@@ -182,10 +199,15 @@ ggplot(toplot, aes(x = UMAP1, y = UMAP2, color = Treatment)) +
   geom_point() +
   theme_classic()
 
+ggsave("PDACbaseumap.pdf", height = 5, width = 6)
+
 # 0.9637
 baseline.roc <- classify(baseline %>% mutate(target = str_extract(baseline$id, "(T|U)")) %>% select(-id))
 baseline.roc$auc
 
 baseline.mr <- model_reliance(baseline %>% mutate(target = str_extract(baseline$id, "(T|U)")) %>%
-                                mutate(target = as.factor(ifelse(target == "T", "CRT", "Untreated"))) %>%
-                                select(-id))
+  mutate(target = as.factor(ifelse(target == "T", "CRT", "Untreated"))) %>%
+  select(-id))
+
+ggsave("PDACbasereliance.pdf", height = 6, width = 5)
+ 
